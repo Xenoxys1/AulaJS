a199d526de5c0b4d1612c8367d54d00325fae4c5
Xenoxys1 <arthurmoraes.gamer@gmail.com>
2024-05-28 17:10:41 -0300

    Aula 7 update

---
+++ js/aula7.js
@@ -0,0 +1,15 @@
+function converterDolar(valor) {
+    let dolar = valor / 5.16;
+    return dolar;
+}
+
+function mostrarResultado(nome, valor) {
+    document.write(`O valor do produto ${nome} em reais é R$${valor}
+     convertido para dolar é $${converterDolar(valor).toFixed(2)}`);
+}
+
+function pegarValor() {
+    const nomeProd = document.getElementById('nome');
+    const reais = document.getElementById('real');
+    mostrarResultado(nomeProd.value, reais.value);
+}
